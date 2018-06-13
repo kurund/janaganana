7446144201752ca7b67aff6bf7d3bc5967b2eed9
shahin.raza <shahin.raza@webaccessglobal.com>
2018-06-13 15:31:02 +0530

    updated the map

--- sql/population_area.sql
+++ sql/population_area.sql
@@ -28,7 +28,7 @@ CREATE TABLE area_sex (
     area character varying(10) NOT NULL,
     sex character varying(10) NOT NULL,
     total integer NOT NULL,
-    geo_version character varying(100) NULL
+    geo_version character varying(100) DEFAULT ''::character varying NOT NULL
 );
 
 
